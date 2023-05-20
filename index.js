71909656a1bb0341bbb06fa4e5dd003a4c66d492
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-05-20 17:02:03 -0300

    feet: Switch para monitorar qual verbo http está sendo chamado

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const fs = require("fs");
 
 const servidor = http.createServer((req, res) => {
     if (req.url == "/produtos") {
-
+        switch (req.method) {
+            case "GET":
+                
+                break;
+        
+            case "POST":
+                break;
+        }
     }else {
         res.writeHead(200, {"Content-Type": "text/plain; charset: utf-8;"});
         res.end("O endpoint padrão é '/produtos'")
