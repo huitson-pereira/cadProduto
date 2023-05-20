83fdd7aeb4a29b33cc25b7347543fe4791b6934b
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-05-20 17:48:07 -0300

    feet: Criando a função para retornar a lista de produtos

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const http = require("http");
 const fs = require("fs");
 
+function listarProdutos(){
+    try{
+        const dadosProd = JSON.parse(fs.readFileSync("dados.json", "utf-8"))
+        return JSON.stringify(dadosProd.produtos)
+    }catch(e){
+        return "Erro ao executar"
+    }
+}
+
 const servidor = http.createServer((req, res) => {
     if (req.url == "/produtos") {
         switch (req.method) {
